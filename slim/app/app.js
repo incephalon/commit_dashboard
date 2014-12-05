83890694170293b2afc574d4aac78da0c57e21bf
Sunder Magar <sunder@amaljamaee.com>
2014-12-05 18:22:59 +0530

    responsive navigation fixes.

--- slim/app/app.js
+++ slim/app/app.js
@@ -106,7 +106,6 @@ app.controller("NavCtrl", ["$scope", function ($scope) {
 
 }]);
 
-
 app.controller('dashboardController', function ($scope) {
 
 });
@@ -114,56 +113,6 @@ app.controller('dashboardController', function ($scope) {
 app.controller('homeController', function ($scope) {
     $scope.welcomeMessage = "Welcome to Home";
 });
-
-
-app.directive("collapseNav", [
-            function () {
-                return {
-                    restrict: "A",
-                    link: function (scope, ele) {
-                        var $a, $aRest, $app, $lists, $listsRest, $nav, $window, Timer, prevWidth, updateClass;
-                        return $window = $(window), $lists = ele.find("ul").parent("li"), $lists.append('<i class="ti-angle-down icon-has-ul-h"></i><i class="ti-angle-double-right icon-has-ul"></i>'), $a = $lists.children("a"), $listsRest = ele.children("li").not($lists), $aRest = $listsRest.children("a"), $app = $("#app"), $nav = $("#nav-container"), $a.on("click", function (event) {
-                            var $parent, $this;
-                            return $app.hasClass("nav-collapsed-min") || $nav.hasClass("nav-horizontal") && $window.width() >= 768 ? !1 : ($this = $(this), $parent = $this.parent("li"), $lists.not($parent).removeClass("open").find("ul").slideUp(), $parent.toggleClass("open").find("ul").stop().slideToggle(), event.preventDefault())
-                        }), $aRest.on("click", function () {
-                            return $lists.removeClass("open").find("ul").slideUp()
-                        }), scope.$on("nav:reset", function () {
-                            return $lists.removeClass("open").find("ul").slideUp()
-                        }), Timer = void 0, prevWidth = $window.width(), updateClass = function () {
-                            var currentWidth;
-                            return currentWidth = $window.width(), 768 > currentWidth && $app.removeClass("nav-collapsed-min"), 768 > prevWidth && currentWidth >= 768 && $nav.hasClass("nav-horizontal") && $lists.removeClass("open").find("ul").slideUp(), prevWidth = currentWidth
-                        }, $window.resize(function () {
-                            var t;
-                            return clearTimeout(t), t = setTimeout(updateClass, 300)
-                        })
-                    }
-                }
-            }
-]);
-
-app.directive("highlightActive", [
-            function () {
-                return {
-                    restrict: "A",
-                    controller: ["$scope", "$element", "$attrs", "$location",
-                        function ($scope, $element, $attrs, $location) {
-                            var highlightActive, links, path;
-                            return links = $element.find("a"), path = function () {
-                                return $location.path()
-                            }, highlightActive = function (links, path) {
-                                return path = "#" + path, angular.forEach(links, function (link) {
-                                    var $li, $link, href;
-                                    return $link = angular.element(link), $li = $link.parent("li"), href = $link.attr("href"), $li.hasClass("active") && $li.removeClass("active"), 0 === path.indexOf(href) ? $li.addClass("active") : void 0
-                                })
-                            }, highlightActive(links, $location.path()), $scope.$watch(path, function (newVal, oldVal) {
-                                return newVal !== oldVal ? highlightActive(links, $location.path()) : void 0
-                            })
-                        }
-                    ]
-                }
-            }
-]);
-
 
 app.controller('myC', function ($scope) {
 
